3f3d0a924cd74ba7bcb58809af88ff2a6c394bcf
akvine <redgooseman55@gmail.com>
2023-04-10 00:06:42 +0300

    add security and otp views

---
+++ src/main/java/ru/akvine/prorisefront/views/OtpView.java
@@ -0,0 +1,51 @@
+package ru.akvine.prorisefront.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route("otp")
+public class OtpView extends VerticalLayout {
+
+    private TextField[] otpFields = new TextField[4];
+    private int currentFieldIndex = 0;
+
+    public OtpView() {
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        H1 title = new H1("Enter your OTP");
+
+        for (int i = 0; i < 4; i++) {
+            otpFields[i] = new TextField();
+            otpFields[i].setMaxLength(1);
+            otpFields[i].setWidth("50px");
+            otpFields[i].setHeight("50px");
+            otpFields[i].setPattern("[0-9]");
+            otpFields[i].setPreventInvalidInput(true);
+            otpFields[i].addValueChangeListener(e -> {
+                if (e.getValue().length() > 0) {
+                    currentFieldIndex++;
+                    if (currentFieldIndex < 4) {
+                        otpFields[currentFieldIndex].focus();
+                    } else {
+                        String otp = "";
+                        for (int j = 0; j < 4; j++) {
+                            otp += otpFields[j].getValue();
+                        }
+                        Notification.show("Your OTP is: " + otp);
+                    }
+                }
+            });
+        }
+
+        HorizontalLayout otpLayout = new HorizontalLayout(otpFields);
+        otpLayout.setSpacing(true);
+        otpLayout.setAlignItems(Alignment.CENTER);
+
+        add(title, otpLayout);
+    }
+
+}
---
+++ src/main/java/ru/akvine/prorisefront/views/SecurityView.java
@@ -0,0 +1,57 @@
+package ru.akvine.prorisefront.views;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route("/security")
+public class SecurityView extends VerticalLayout {
+
+    private boolean isRegistration = false;
+
+    private TextField usernameField = new TextField("Username");
+    private PasswordField passwordField = new PasswordField("Password");
+    private TextField emailField = new TextField("Email");
+    private TextField fullNameField = new TextField("Full Name");
+
+    public SecurityView() {
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        H1 title = new H1("Welcome to My App");
+
+        Button loginButton = new Button("Login");
+        Button registerButton = new Button("Register");
+
+        loginButton.addClickListener(e -> {
+            if (isRegistration) {
+                isRegistration = false;
+                registerButton.removeClassName("active");
+                loginButton.addClassName("active");
+                removeAll();
+                add(title, usernameField, passwordField);
+            }
+        });
+
+        registerButton.addClickListener(e -> {
+            if (!isRegistration) {
+                isRegistration = true;
+                loginButton.removeClassName("active");
+                registerButton.addClassName("active");
+                removeAll();
+                add(title, usernameField, passwordField, emailField, fullNameField);
+            }
+        });
+
+        HorizontalLayout buttonLayout = new HorizontalLayout(loginButton, registerButton);
+        buttonLayout.setSpacing(true);
+        loginButton.addClassName("active");
+
+        add(title, usernameField, passwordField, buttonLayout);
+
+    }
+
+}
