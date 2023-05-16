b716b661b5813afac2a1da1dfc60f89185f11562
akvine <redgooseman55@gmail.com>
2023-05-16 21:59:14 +0300

    add routing if exit from app

--- src/main/java/ru/akvine/prorisefront/components/Header.java
+++ src/main/java/ru/akvine/prorisefront/components/Header.java
@@ -1,6 +1,7 @@
 package ru.akvine.prorisefront.components;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -10,6 +11,7 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.RouterLink;
 import ru.akvine.prorisefront.views.main.*;
+import ru.akvine.prorisefront.views.security.SecurityView;
 
 @CssImport(value = "./styles/header.css", themeFor = "vaadin-app-layout")
 public class Header extends AppLayout {
@@ -20,23 +22,34 @@ public class Header extends AppLayout {
     public Component buildHeader() {
         Image logo = new Image("https://via.placeholder.com/150x50?text=Prorise", "Prorise");
 
-        // Список ссылок на страницы
-        RouterLink page1Link = new RouterLink("Показатели", KPIView.class);
-        RouterLink page5Link = new RouterLink("Сотрудники", KPIView.class);
-        RouterLink page2Link = new RouterLink("Задачи", EmployersView.class);
-        RouterLink page3Link = new RouterLink("Учет времени", TimeTrackerView.class);
-        RouterLink page4Link = new RouterLink("Настройки профиля", ProfileSettingsView.class);
+        RouterLink kpiViewLink = new RouterLink("Показатели", KPIView.class);
+        RouterLink employersViewLink = new RouterLink("Сотрудники", EmployersView.class);
+        RouterLink hrPageViewLink = new RouterLink("Служба обратной связи", HRPage.class);
+        RouterLink timeTrackerViewLink = new RouterLink("Учет времени", TimeTrackerView.class);
+        RouterLink reportsViewLink = new RouterLink("Генерация отчетов", ReportsView.class);
+        RouterLink trainingViewLink = new RouterLink("Обучение и достижения", TrainingView.class);
+        RouterLink profileSettingsView = new RouterLink("Настройки профиля", ProfileSettingsView.class);
 
-        // Кнопка "Sign In"
         Button signInButton = new Button("Выйти");
         signInButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        signInButton.addClickListener(buttonClickEvent -> {
+            RouterLink link = new RouterLink("", SecurityView.class);
+            UI.getCurrent().getPage().executeJs("window.open($0)", link.getHref());
+        });
 
-        // Расположение элементов в header
-        HorizontalLayout headerContent = new HorizontalLayout(logo, page5Link, page1Link, page2Link, page3Link, page4Link, signInButton);
+        HorizontalLayout headerContent = new HorizontalLayout(logo,
+                kpiViewLink,
+                employersViewLink,
+                reportsViewLink,
+                hrPageViewLink,
+                trainingViewLink,
+                timeTrackerViewLink,
+                profileSettingsView,
+                signInButton
+        );
         headerContent.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         headerContent.setWidthFull();
 
-        // Конфигурация AppLayout
         AppLayout appLayout = new AppLayout();
         appLayout.setPrimarySection(AppLayout.Section.NAVBAR);
         appLayout.addToNavbar(headerContent);
